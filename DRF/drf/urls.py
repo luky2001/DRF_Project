847b5d771ff0f1ce7741e084532f17e0f0187e07
Rohit <luckykumarpatel2001@gmail.com>
2024-08-10 21:35:31 +0530

    delete

--- DRF/drf/urls.py
+++ DRF/drf/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('/logout/',views.logout,name='logout'),
     path('/profile/',views.profile,name='profile'),
     path('/update/',views.update,name='update'),
+    path('/delete/',views.delete,name='delete'),
 ]
