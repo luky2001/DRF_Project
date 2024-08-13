e179949512efe06e0072125cbc6e19e8f1324d22
Rohit <luckykumarpatel2001@gmail.com>
2024-08-13 21:15:11 +0530

    DRF

--- DRF/drf/models.py
+++ DRF/drf/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Company(models.Model):
+	name=models.CharField(max_length=50)
+	location=models.CharField(max_length=100)
+	about=models.TextField()
+	type=models.CharField(max_length=10)
+	
+
 
