05c344b88708493dde1d1eb643f866ea22d3f577
Rohit <luckykumarpatel2001@gmail.com>
2024-08-17 14:42:59 +0530

    login

--- DRF/drf/urls.py
+++ DRF/drf/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    #path('/login/',views.login,name='login'),
+    path('/login/',views.login,name='login'),
     #path('/logout/',views.logout,name='logout'),
     #path('/profile/',views.profile,name='profile'),
     #path('/update/',views.update,name='update'),
