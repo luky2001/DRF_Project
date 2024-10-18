2c89d087869b55375c52a20a807a3da3ccd7d04a
Rohit <luckykumarpatel2001@gmail.com>
2024-10-18 12:34:47 +0530

    delete

--- DRF/drf/views.py
+++ DRF/drf/views.py
@@ -1,26 +1,3 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request,'home.html')
-def index(request):
-    return render(request,'base.html')
-def update(request):
-    return render(request,'update.html')
-def delete(request):
-    return render(request,'delete')
-def profile(request):
-    return render(request,'profile')
-def customer(request):
-    return render(request,'customer.html')
-def product(request):
-    return render(request,'product')
-def show(request):
-    return render(request,'show.html')
-def login(request):
-    return render(request,'lohin.html')
-def logout(request):
-    return render(request,'logout')
-def list(request):
-    return render(request,'list')
-def register(request):
-    return render(request,'register.html')
+def
